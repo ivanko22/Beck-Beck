a47a215261f566b4ed51102bc17e9aef7947c584
ivankome <ivan.kordonets@cfusacorp.com>
2025-09-02 13:26:20 -0400

    remove storybook examples

--- beckui/src/components/typography/Typography.stories.tsx
+++ beckui/src/components/typography/Typography.stories.tsx
@@ -9,6 +9,6 @@ export default {
   },
 };
 
-export const TitleSso = () => <Typography variant="titleSso" color='var(--primary-color)'>Sign In</Typography>;
+export const TitleSso = () => <Typography variant="title" color='var(--primary-color)'>Sign In</Typography>;
 
 export const Subtitle = () => <Typography variant="subtitle">Welcome back</Typography>;
